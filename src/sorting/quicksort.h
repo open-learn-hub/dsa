88441f48660a07a9c18c8c51bd094876691d30e0
ndtoan <ndinhtoan48@gmail.com>
2024-12-16 00:04:08 +0700

    Template for signature

---
+++ src/sorting/quicksort.h
@@ -0,0 +1,16 @@
+#ifndef QUICK_SORT_H_OLH
+#define QUICK_SORT_H_OLH
+
+ /******************************************************
+ * File: quicksort.cpp
+ * Author: Nguyen Dinh Toan
+ * Date: 2024-12-15
+ * Description: Implementation of Quick Sort Algorithm
+ * Reference: https://www.geeksforgeeks.org/quick-sort-algorithm/
+ ******************************************************/
+
+namespace olh
+{
+
+}
+#endif
